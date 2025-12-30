c836bde6f3e2a284f5a7b4817767f7355ab31c98
Patrolin <patrik@balas.cz>
2025-12-30 18:33:23 +0100

    c

--- src/utils/benchmark.h
+++ src/utils/benchmark.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "definitions.h"
 
-#define optimizer_read_write_fence(value) asm volatile("" : "+x"(value))
-#define optimizer_read_fence(value) asm volatile("" : "x"(value))
+#define optimizer_fence(value) asm volatile("" : "+x"(value))
 // TODO: timings
