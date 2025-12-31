f26c801dfac2838a608dac8865a3c15b68515d3b
Patrolin <patrik@balas.cz>
2025-12-31 18:22:59 +0100

    c

--- src/utils/math_exact.h
+++ src/utils/math_exact.h
@@ -3,40 +3,48 @@
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
-// Integer
+// Integer slice
 typedef struct {
-  /* NOTE: two's complement */
+  /* NOTE: two's complement chunks */
   u64* chunks;
   u64 chunks_size;
 } Integer;
-static u64 integer_is_negative(Integer a) {
-  return a.chunks[a.chunks_size - 1] >> 63;
-}
-static u64 integer_get_chunk(Integer a, intptr i) {
-  u64 sign_extension = integer_is_negative(a) ? u64(-1) : 0;
-  return i < a.chunks_size ? a.chunks[i] : sign_extension;
-}
+#define integer_sign_extension(a) (a.chunks[a.chunks_size - 1] >> 63 ? u64(-1) : 0)
+#define integer_get_chunk(a, i, sign_extension) (i < a.chunks_size ? a.chunks[i] : sign_extension)
 #define integer_alloc(chunks_size) {arena_alloc_array(global_arena, u64, chunks_size), chunks_size}
 Integer integer_alloc_sign_extend(Integer a, u64 new_chunks_size) {
   if (expect_unlikely(a.chunks_size >= new_chunks_size)) return a;
   Integer result = integer_alloc(new_chunks_size);
+  /* NOTE: split into two loops, so that optimizer can unroll them */
   intptr i = 0;
   do {
     result.chunks[i] = a.chunks[i];
   } while (++i < a.chunks_size);
-  u64 sign_extension = integer_is_negative(a) > MAX(i64) ? u64(-1) : 0;
-  do {
-    result.chunks[i] = sign_extension;
-  } while (++i < new_chunks_size);
+  u64 sign_extension = integer_sign_extension(a);
+  while (i < new_chunks_size) {
+    result.chunks[i++] = sign_extension;
+  }
   return result;
 }
-Integer integer_alloc_negate(Integer a) {
+Integer integer_alloc_not(Integer a) {
   Integer result = integer_alloc(a.chunks_size);
   intptr i = 0;
   u64 carry = 1;
   do {
+    result.chunks[i] = ~a.chunks[i];
+  } while (++i < a.chunks_size);
+  return result;
+}
+Integer integer_alloc_negate(Integer a) {
+  /* NOTE: negate can overflow in two's complement... */
+  Integer result = integer_alloc(a.chunks_size + 1);
+  intptr i = 0;
+  u64 carry = 1;
+  do {
+    /* NOTE: add_overflow() cannot be unrolled */
     carry = (u64)add_overflow(~a.chunks[i], carry, &result.chunks[i]);
   } while (++i < a.chunks_size);
+  // assert(carry == 0);
   return result;
 }
 Integer integer_alloc_add(Integer a, Integer b) {
@@ -44,8 +52,11 @@ Integer integer_alloc_add(Integer a, Integer b) {
   Integer result = integer_alloc_sign_extend(a, max_chunks_size);
   intptr i = 0;
   u64 carry = 0;
+  u64 sign_extension = integer_sign_extension(b);
   do {
-    result.chunks[i] = add_with_carry(result.chunks[i], integer_get_chunk(b, i), carry, &carry);
+    /* NOTE: add_with_carry() cannot be unrolled */
+    u64 b_chunk = integer_get_chunk(b, i, sign_extension);
+    result.chunks[i] = add_with_carry(result.chunks[i], b_chunk, carry, &carry);
   } while (++i < max_chunks_size);
   return result;
 }
@@ -54,8 +65,11 @@ Integer integer_alloc_sub(Integer a, Integer b) {
   Integer result = integer_alloc_sign_extend(a, max_chunks_size);
   intptr i = 0;
   u64 borrow = 0;
+  u64 sign_extension = integer_sign_extension(b);
   do {
-    result.chunks[i] = sub_with_borrow(result.chunks[i], integer_get_chunk(b, i), borrow, &borrow);
+    /* NOTE: sub_with_borrow() cannot be unrolled */
+    u64 b_chunk = integer_get_chunk(b, i, sign_extension);
+    result.chunks[i] = sub_with_borrow(result.chunks[i], b_chunk, borrow, &borrow);
   } while (++i < max_chunks_size);
   return result;
 }
@@ -79,17 +93,23 @@ Integer _integer_alloc_karatsuba_mul(Integer a, Integer b) {
     Integer result_1 = _integer_alloc_karatsuba_mul(integer_alloc_add(A, C), integer_alloc_add(B, D));
     result_1 = integer_alloc_sub(result_1, result_0);
     result_1 = integer_alloc_sub(result_1, result_2);
-    // merge results
+    // merge result_0
     Integer result = integer_alloc_sign_extend(result_0, a.chunks_size * 2);
+    // merge result_1
     intptr i = split;
     u64 carry = 0;
+    u64 sign_extension = integer_sign_extension(result_1);
     do {
-      result.chunks[i] = add_with_carry(result.chunks[i], integer_get_chunk(result_1, i - split), carry, &carry);
+      u64 result_1_chunk = integer_get_chunk(result_1, i - split, sign_extension);
+      result.chunks[i] = add_with_carry(result.chunks[i], result_1_chunk, carry, &carry);
     } while (++i < result.chunks_size);
+    // merge result_2
     i = split * 2;
     carry = 0;
+    sign_extension = integer_sign_extension(result_2);
     do {
-      result.chunks[i] = add_with_carry(result.chunks[i], integer_get_chunk(result_1, i - split * 2), carry, &carry);
+      u64 result_2_chunk = integer_get_chunk(result_2, i - split * 2, sign_extension);
+      result.chunks[i] = add_with_carry(result.chunks[i], result_2_chunk, carry, &carry);
     } while (++i < result.chunks_size);
     return result;
   }
